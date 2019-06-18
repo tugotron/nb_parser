ab370179c935aaff0cbd92e590d77996e8ec92f0
viktor <viktor.savelev@3d4medical.com>
2019-06-19 00:04:44 +0300

    Implementing jobs;

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 import logging
 import requests
 from lxml import html
-from telegram.ext import Updater, CommandHandler
-
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -16,10 +15,11 @@ class Parser:
 
     def format_message(self, arr):
         res = ''
-        for k in arr:
-            res += f'{k}\n'
-            res += '\n'.join([f'▫️ {v}' for v in arr[k]])
-            res += '\n\n'
+        for k, v in arr.items():
+            res += f'▫️ {k} - {v}\n'
+            # res += f'{k}\n'
+            # res += '\n'.join([f'▫️ {v}' for v in arr[k]])
+            # res += '\n\n'
         return res
 
     def get_sessions(self):
@@ -38,12 +38,13 @@ class Parser:
             location = self.format_header(i.getchildren()[1].text)
 
             # get detailed info
-            link = i.getchildren()[2].find('a').get('href')
-            details_page = requests.get(link)
-            details_tree = html.fromstring(details_page.text)
-            li = [li.text_content() for li in details_tree.cssselect('div[id=block-system-main] ul li')]
+            # link = i.getchildren()[2].find('a').get('href')
+            # details_page = requests.get(link)
+            # details_tree = html.fromstring(details_page.text)
+            # li = [li.text_content() for li in details_tree.cssselect('div[id=block-system-main] ul li')]
+            # result[f'{datetime} - {location}'] = li
 
-            result[f'{datetime} - {location}'] = li
+            result[f'{datetime}'] = location
 
         return self.format_message(result)
 
@@ -52,27 +53,42 @@ class Bot:
     def __init__(self):
         self.updater = Updater(token=os.environ['BOT_TOKEN'])
         self.dispatcher = self.updater.dispatcher
+        self.last_update = Parser().get_sessions()
 
     def start(self, update, context):
-        sessions = Parser()
-        data = sessions.get_sessions()
-        update.send_message(update.message.chat_id, data)
+        self.updater.bot.send_message(context.message.chat_id, 'start handler')
 
     def help(self, update, context):
         pass
+
+    def sessions(self, update, context):
+        data = self.last_update
+        self.updater.bot.send_message(context.message.chat_id, data)
+
+    # def sayhi(self, bot, job):
+    #     job.context.message.reply_text("hi")
+
+    def time(self, bot, update, job_queue):
+        job = job_queue.run_repeating(self.sessions, 5, context=update)
 
     def handlers(self):
         start_handler = CommandHandler('start', self.start)
         self.dispatcher.add_handler(start_handler)
 
         help_handler = CommandHandler('help', self.help)
-        self.dispatcher.add_handler(help_handler )
+        self.dispatcher.add_handler(help_handler)
+
+        sessions_handler = CommandHandler('sessions', self.sessions)
+        self.dispatcher.add_handler(sessions_handler)
+
+        message_handler = CommandHandler('start', self.time, pass_job_queue=True)
+        self.dispatcher.add_handler(message_handler)
 
     def run(self):
         self.handlers()
 
         self.updater.start_polling()
-        self.updater.idle()
+        # self.updater.idle()
 
 
 if __name__ == '__main__':
